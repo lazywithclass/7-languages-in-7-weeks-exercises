0349d8e6701475951f019f5f3d25be380ae3b831
lazywithclass <montecristoh@gmail.com>
2015-02-16 13:40:28 +0000

    finished day exercises

--- lua/day1.lua
+++ lua/day1.lua
@@ -35,4 +35,31 @@ print_first_primes_ending_in_3(10)
 print("first 33 numbers that are prime and end in 3")
 print_first_primes_ending_in_3(33)
 
+function for_loop(a, b, f)
+  if a <= b then
+    f()
+    for_loop(a + 1, b, f)
+  end
+end
 
+print("call a passed function b - a times")
+function printer() print('printing') end
+for_loop(1, 10, printer)
+
+function reduce(max, init, f)
+  if max >= 1 then
+    return reduce(max - 1, f(max, init), f)
+  else
+    return init
+  end
+end
+
+function add(previous, next) return previous + next end
+print("the sum of numbers from 0 to 100: " .. reduce(100, 0, add))
+
+function mult(previous, next) return previous * next end
+function factorial(n)
+  return reduce(n, 1, mult)
+end
+
+print("the factorial of 7 is: " .. factorial(7))
